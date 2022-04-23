da4888cb637f5899ee2e325d584daa7936813af8
novacooo <jaceknowak226@gmail.com>
2022-04-23 17:16:23 +0200

    Added menu for user

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -3,12 +3,22 @@ import {
   Button,
   Flex,
   HStack,
+  IconButton,
+  Menu,
+  MenuButton,
+  MenuDivider,
+  MenuItem,
+  MenuList,
   StackDivider,
   Text,
+  Tooltip,
   useColorMode,
   useColorModeValue,
 } from '@chakra-ui/react';
-import { ReactComponent as Logo } from 'assets/logo_mariall.svg';
+import { SunIcon, MoonIcon, ChevronDownIcon } from '@chakra-ui/icons';
+import { FiLogOut, FiSettings } from 'react-icons/fi';
+import { ReactComponent as LogoLight } from 'assets/logo_light.svg';
+import { ReactComponent as LogoDark } from 'assets/logo_dark.svg';
 
 const data = {
   userName: 'Jacek Nowak',
@@ -17,28 +27,64 @@ const data = {
 
 const TopBar = () => {
   const { colorMode, toggleColorMode } = useColorMode();
-  const bg = useColorModeValue('gray.100', 'gray.700');
-  const color = useColorModeValue('white', 'gray.800');
+
+  const bgColor = useColorModeValue('white', 'gray.700');
+  const textColor = useColorModeValue('gray.600', 'gray.300');
+
+  const logoHeight = 36;
 
   return (
     <Flex
-      px={8}
-      py={4}
+      px={6}
+      py={3}
       justify="space-between"
       align="center"
-      borderBottomWidth="1px"
-      bgColor={bg}
+      boxShadow="base"
+      bgColor={bgColor}
+      borderBottomWidth={1}
     >
       <HStack spacing={5} divider={<StackDivider />}>
-        <Logo height={36} />
-        <Text>{data.appTitle}</Text>
+        {colorMode === 'light' ? (
+          <LogoLight height={logoHeight} />
+        ) : (
+          <LogoDark height={logoHeight} />
+        )}
+        <Text color={textColor}>{data.appTitle}</Text>
       </HStack>
-      <Button onClick={toggleColorMode}>
-        Toggle {colorMode === 'light' ? 'dark' : 'light'}
-      </Button>
       <HStack spacing={3}>
-        <Text>{data.userName}</Text>
-        <Avatar name={data.userName} size="sm" />
+        <Tooltip label="Przełącz tryb">
+          <IconButton
+            onClick={toggleColorMode}
+            variant="ghost"
+            aria-label="Toggle theme"
+            icon={colorMode === 'light' ? <SunIcon /> : <MoonIcon />}
+          />
+        </Tooltip>
+        <Menu size="xs">
+          <Tooltip label="Pokaż menu">
+            <MenuButton
+              as={Button}
+              variant="ghost"
+              rightIcon={<ChevronDownIcon />}
+            >
+              <HStack spacing={2}>
+                <Text color={textColor} fontSize="xs">
+                  {data.userName}
+                </Text>
+                <Avatar name={data.userName} size="xs" />
+              </HStack>
+            </MenuButton>
+          </Tooltip>
+          <MenuList>
+            <MenuItem icon={<FiSettings />} fontSize="sm">
+              Ustawienia użytkownika
+            </MenuItem>
+            <MenuDivider />
+            <MenuItem icon={<FiLogOut />} fontSize="sm" color="red.500">
+              Wyloguj się
+            </MenuItem>
+          </MenuList>
+        </Menu>
       </HStack>
     </Flex>
   );
