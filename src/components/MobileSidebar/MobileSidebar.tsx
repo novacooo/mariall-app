7647fa76c0bf2921ac447c69eeb9d7cddbc03030
novacooo <jaceknowak226@gmail.com>
2022-05-02 22:24:48 +0200

    Added content to mobile sidebar

--- src/components/MobileSidebar/MobileSidebar.tsx
+++ src/components/MobileSidebar/MobileSidebar.tsx
@@ -1,27 +1,78 @@
 import {
+  Divider,
   Drawer,
   DrawerBody,
-  DrawerCloseButton,
   DrawerContent,
-  DrawerHeader,
-  DrawerOverlay,
   Text,
+  DrawerOverlay,
+  Flex,
+  Avatar,
+  useColorModeValue,
+  Tooltip,
+  IconButton,
 } from '@chakra-ui/react';
+import ColorButton from 'components/ColorButton/ColorButton';
+import ColorModeButton from 'components/ColorModeButton/ColorModeButton';
+import LanguageButton from 'components/LanguageButton/LanguageButton';
+import SidebarTabs from 'components/SidebarTabs/SidebarTabs';
+import { useTranslation } from 'react-i18next';
+import { FiLogOut } from 'react-icons/fi';
+import { useNavigate } from 'react-router-dom';
+import { routes } from 'routes';
 
 interface MobileSidebarProps {
   isOpen: boolean;
   onClose: () => void;
 }
 
+const data = {
+  userName: 'Jacek Nowak',
+};
+
 const MobileSidebar = ({ isOpen, onClose }: MobileSidebarProps) => {
+  const { t } = useTranslation();
+  const navigate = useNavigate();
+
+  const logoutIconColor = useColorModeValue('red.500', 'red.400');
+  const bgColor = useColorModeValue('white', 'gray.800');
+
   return (
-    <Drawer isOpen={isOpen} placement="left" onClose={onClose}>
+    <Drawer
+      isOpen={isOpen}
+      placement="left"
+      onClose={onClose}
+      autoFocus={false}
+    >
       <DrawerOverlay />
-      <DrawerContent>
-        <DrawerCloseButton />
-        <DrawerHeader>Menu</DrawerHeader>
-        <DrawerBody>
-          <Text>MobileSidebar</Text>
+      <DrawerContent bg={bgColor}>
+        <DrawerBody px={3} py={6}>
+          <Flex minH="100%" direction="column" gap={6}>
+            <Flex px={3} align="center" justify="space-between">
+              <Flex gap={4} align="center">
+                <Avatar name={data.userName} size="md" />
+                <Text fontSize="lg">{data.userName}</Text>
+              </Flex>
+              <Tooltip label={t('tooltips.signOut')}>
+                <IconButton
+                  variant="outline"
+                  aria-label={t('tooltips.signOut')}
+                  color={logoutIconColor}
+                  icon={<FiLogOut />}
+                  onClick={() => navigate(routes.login)}
+                />
+              </Tooltip>
+            </Flex>
+            <Divider mx={3} w="auto" />
+            <Flex direction="column" flexGrow={1}>
+              <SidebarTabs />
+            </Flex>
+            <Divider mx={3} w="auto" />
+            <Flex gap={3} justify="center">
+              <LanguageButton />
+              <ColorButton />
+              <ColorModeButton />
+            </Flex>
+          </Flex>
         </DrawerBody>
       </DrawerContent>
     </Drawer>
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,110 +1,18 @@
 import { Flex, useBreakpointValue, useColorModeValue } from '@chakra-ui/react';
-import SidebarItem from 'components/SidebarItem/SidebarItem';
-import SidebarMenu from 'components/SidebarMenu/SidebarMenu';
-import { SidebarGroup, SidebarTab } from 'constants/sidebar';
-import { useTranslation } from 'react-i18next';
-import { useLocation } from 'react-router-dom';
-import { routes } from 'routes';
-
-interface IPanelTab {
-  id: string;
-  groupId: SidebarGroup;
-  tabId: SidebarTab;
-  route: string;
-  admin: boolean;
-}
+import SidebarTabs from 'components/SidebarTabs/SidebarTabs';
 
 interface SidebarProps {
   width: number | string;
   showAlways?: boolean;
 }
 
-const groups: SidebarGroup[] = [
-  SidebarGroup.PRODUCTS,
-  SidebarGroup.SALARIES,
-  SidebarGroup.EMPLOYEES,
-  SidebarGroup.OTHER,
-];
-
-const tabs: IPanelTab[] = [
-  {
-    id: 'panel-tab-01',
-    groupId: SidebarGroup.PRODUCTS,
-    tabId: SidebarTab.ADDING_QUANTITY,
-    route: routes.panelAddingQuantity,
-    admin: false,
-  },
-  {
-    id: 'panel-tab-02',
-    groupId: SidebarGroup.PRODUCTS,
-    tabId: SidebarTab.PRODUCTS_MANAGEMENT,
-    route: routes.panelProductsManagement,
-    admin: true,
-  },
-  {
-    id: 'panel-tab-03',
-    groupId: SidebarGroup.SALARIES,
-    tabId: SidebarTab.SALARIES,
-    route: routes.panelSalaries,
-    admin: true,
-  },
-  {
-    id: 'panel-tab-04',
-    groupId: SidebarGroup.SALARIES,
-    tabId: SidebarTab.PRINTING_SUMMARIES,
-    route: routes.panelPrinting,
-    admin: true,
-  },
-  {
-    id: 'panel-tab-05',
-    groupId: SidebarGroup.EMPLOYEES,
-    tabId: SidebarTab.EMPLOYEES_MANAGEMENT,
-    route: routes.panelEmployeesManagement,
-    admin: true,
-  },
-  {
-    id: 'panel-tab-06',
-    groupId: SidebarGroup.OTHER,
-    tabId: SidebarTab.LOGS,
-    route: routes.panelLogs,
-    admin: true,
-  },
-];
-
 const Sidebar = ({ width, showAlways = false }: SidebarProps) => {
   const bgColor = useColorModeValue('white', 'gray.800');
-  const location = useLocation();
-  const { t } = useTranslation();
 
   const sidebarDisplay = useBreakpointValue({
     base: 'none',
     md: 'flex',
   });
-
-  const getGroupName = (groupId: SidebarGroup) => {
-    if (groupId === SidebarGroup.PRODUCTS) return t('sidebar.groups.products');
-    if (groupId === SidebarGroup.SALARIES) return t('sidebar.groups.salaries');
-    if (groupId === SidebarGroup.EMPLOYEES)
-      return t('sidebar.groups.employees');
-    if (groupId === SidebarGroup.OTHER) return t('sidebar.groups.other');
-
-    return null;
-  };
-
-  const getTabName = (tabId: SidebarTab) => {
-    if (tabId === SidebarTab.ADDING_QUANTITY)
-      return t('sidebar.tabs.addingQuantity');
-    if (tabId === SidebarTab.PRODUCTS_MANAGEMENT)
-      return t('sidebar.tabs.productsManagement');
-    if (tabId === SidebarTab.SALARIES) return t('sidebar.tabs.salaries');
-    if (tabId === SidebarTab.PRINTING_SUMMARIES)
-      return t('sidebar.tabs.printingSummaries');
-    if (tabId === SidebarTab.EMPLOYEES_MANAGEMENT)
-      return t('sidebar.tabs.employeesManagement');
-    if (tabId === SidebarTab.LOGS) return t('sidebar.tabs.logs');
-
-    return null;
-  };
 
   return (
     <Flex
@@ -116,35 +24,7 @@ const Sidebar = ({ width, showAlways = false }: SidebarProps) => {
       bgColor={bgColor}
       borderRightWidth={1}
     >
-      <Flex direction="column" gap={5}>
-        {groups.map((group) => {
-          const groupName = getGroupName(group);
-          if (!groupName) return null;
-
-          return (
-            <SidebarMenu key={groupName} name={groupName}>
-              {tabs.map((tab) => {
-                const tabName = getTabName(tab.tabId);
-                if (!tabName) return null;
-                if (tab.groupId === group) {
-                  return (
-                    <SidebarItem
-                      key={tab.id}
-                      active={
-                        `${routes.panel}/${tab.route}` === location.pathname
-                      }
-                      link={tab.route}
-                    >
-                      {tabName}
-                    </SidebarItem>
-                  );
-                }
-                return null;
-              })}
-            </SidebarMenu>
-          );
-        })}
-      </Flex>
+      <SidebarTabs />
     </Flex>
   );
 };
