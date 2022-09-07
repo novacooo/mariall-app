5420a6095f773e8d84de9f8d02830bc954509075
Jacek Nowak <jaceknowak226@gmail.com>
2022-09-07 16:18:46 +0200

    Setup Apollo client

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -2,6 +2,7 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { HashRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import theme from 'theme/theme';
 import { routes } from 'routes';
 import { Suspense } from 'react';
@@ -12,23 +13,30 @@ import PanelPage from './PanelPage';
 import MenuPage from './MenuPage';
 import LoginPage from './LoginPage';
 
+const apolloClient = new ApolloClient({
+  uri: process.env.REACT_APP_GRAPHQL_URL,
+  cache: new InMemoryCache(),
+});
+
 const App = () => (
   <Suspense fallback="loading">
-    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-    <ChakraProvider theme={theme}>
-      <Provider store={store}>
-        <HashRouter basename="/">
-          <MainTemplate>
-            <Routes>
-              <Route path={routes.menu} element={<MenuPage />} />
-              <Route path={routes.login} element={<LoginPage />} />
-              <Route path={`${routes.panel}/*`} element={<PanelPage />} />
-              <Route path="*" element={<Navigate to={routes.menu} replace />} />
-            </Routes>
-          </MainTemplate>
-        </HashRouter>
-      </Provider>
-    </ChakraProvider>
+    <ApolloProvider client={apolloClient}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <ChakraProvider theme={theme}>
+        <Provider store={store}>
+          <HashRouter basename="/">
+            <MainTemplate>
+              <Routes>
+                <Route path={routes.menu} element={<MenuPage />} />
+                <Route path={routes.login} element={<LoginPage />} />
+                <Route path={`${routes.panel}/*`} element={<PanelPage />} />
+                <Route path="*" element={<Navigate to={routes.menu} replace />} />
+              </Routes>
+            </MainTemplate>
+          </HashRouter>
+        </Provider>
+      </ChakraProvider>
+    </ApolloProvider>
   </Suspense>
 );
 
