ad8820f11649ae9e93f7bbe8f66701e39fef076c
Jacek Nowak <jaceknowak226@gmail.com>
2022-09-08 15:06:20 +0200

    Husky & lint-staged

--- src/hooks/useErrorToast.ts
+++ src/hooks/useErrorToast.ts
@@ -14,6 +14,7 @@ export const useErrorToast = () => {
 
   const errorToast = (error: unknown) => {
     let message = 'Unknown Error';
+
     if (error instanceof Error) message = error.message;
 
     toast({
