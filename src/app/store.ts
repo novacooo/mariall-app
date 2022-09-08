bada590a955b52bd1cb130d09ac6100be140c547
Jacek Nowak <jaceknowak226@gmail.com>
2022-09-08 15:41:32 +0200

    Redux persist

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,32 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import userReducer from 'features/user/userSlice';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
+
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+};
+
+const rootReducer = combineReducers({
+  user: userReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+  devTools: process.env.NODE_ENV !== 'production',
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,9 +1,10 @@
 import { HashRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { routes } from 'routes';
 import { Suspense } from 'react';
-import { store } from 'app';
+import { store, persistor } from 'app';
 import MainTemplate from 'templates/MainTemplate';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import PanelPage from './PanelPage';
 import MenuPage from './MenuPage';
 import LoginPage from './LoginPage';
@@ -11,16 +12,18 @@ import LoginPage from './LoginPage';
 const App = () => (
   <Suspense fallback="loading">
     <Provider store={store}>
-      <HashRouter basename="/">
-        <MainTemplate>
-          <Routes>
-            <Route path={routes.menu} element={<MenuPage />} />
-            <Route path={routes.login} element={<LoginPage />} />
-            <Route path={`${routes.panel}/*`} element={<PanelPage />} />
-            <Route path="*" element={<Navigate to={routes.menu} replace />} />
-          </Routes>
-        </MainTemplate>
-      </HashRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <HashRouter basename="/">
+          <MainTemplate>
+            <Routes>
+              <Route path={routes.menu} element={<MenuPage />} />
+              <Route path={routes.login} element={<LoginPage />} />
+              <Route path={`${routes.panel}/*`} element={<PanelPage />} />
+              <Route path="*" element={<Navigate to={routes.menu} replace />} />
+            </Routes>
+          </MainTemplate>
+        </HashRouter>
+      </PersistGate>
     </Provider>
   </Suspense>
 );
