c34275a179563b39dc541e56db11116dd97bcb98
Jacek Nowak <jaceknowak226@gmail.com>
2022-10-07 01:23:30 +0200

    Locales & icons

--- src/components/ProductDrawer/ProductDrawer.tsx
+++ src/components/ProductDrawer/ProductDrawer.tsx
@@ -7,12 +7,13 @@ import {
   DrawerFooter,
   DrawerHeader,
   DrawerOverlay,
-  Input,
   Spinner,
   Text,
 } from '@chakra-ui/react';
+import { FiSave, FiTrash2 } from 'react-icons/fi';
 import { selectThemeAccentColor } from 'features/theme/themeSlice';
 import { useAppSelector } from 'hooks';
+import { useTranslation } from 'react-i18next';
 
 export interface IDrawerProduct {
   id: string;
@@ -29,6 +30,8 @@ interface ProductDrawerProps {
 }
 
 const ProductDrawer = ({ product, isOpen, onClose }: ProductDrawerProps) => {
+  const { t } = useTranslation();
+
   const themeAccentColor = useAppSelector(selectThemeAccentColor);
 
   return (
@@ -36,7 +39,7 @@ const ProductDrawer = ({ product, isOpen, onClose }: ProductDrawerProps) => {
       <DrawerOverlay />
       <DrawerContent>
         <DrawerCloseButton />
-        <DrawerHeader>Edycja produktu</DrawerHeader>
+        <DrawerHeader>{t('drawers.headers.productEdition')}</DrawerHeader>
         {product ? (
           <>
             <DrawerBody>
@@ -46,9 +49,12 @@ const ProductDrawer = ({ product, isOpen, onClose }: ProductDrawerProps) => {
               <Text>{product.price}</Text>
               <Text>{product.active}</Text>
             </DrawerBody>
-            <DrawerFooter>
-              <Button w="full" colorScheme={themeAccentColor}>
-                Zapisz zmiany
+            <DrawerFooter flexDirection="column" alignItems="stretch" gap={4}>
+              <Button colorScheme={themeAccentColor} rightIcon={<FiSave />}>
+                {t('buttons.saveChanges')}
+              </Button>
+              <Button rightIcon={<FiTrash2 />} colorScheme="red" variant="outline">
+                {t('buttons.deleteProduct')}
               </Button>
             </DrawerFooter>
           </>
