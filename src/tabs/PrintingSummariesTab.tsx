b5e7e409242622aae1fddad7ec43640128499a9c
Jacek Nowak <jaceknowak226@gmail.com>
2022-10-16 00:10:09 +0200

    Move worker selects to printing summaries tab

--- src/tabs/PrintingSummariesTab.tsx
+++ src/tabs/PrintingSummariesTab.tsx
@@ -1,11 +1,37 @@
+import { useRef, useState } from 'react';
+import { Spinner, Text } from '@chakra-ui/react';
+
+import WorkerSelects, { IWorkerSelectsData, WorkerSelectsHandle } from 'components/WorkerSelects/WorkerSelects';
+import { useGetEmployeesQuery } from 'graphql/generated/schema';
+import { useErrorToast } from 'hooks';
 import ProtectedTabTemplate from 'templates/ProtectedTabTemplate';
 
-const PrintingSummariesTab = () => (
-  <ProtectedTabTemplate>
-    <div>
-      <p>printing-summaries-tab</p>
-    </div>
-  </ProtectedTabTemplate>
-);
+const PrintingSummariesTab = () => {
+  const errorToast = useErrorToast();
+  const workerSelectsRef = useRef<WorkerSelectsHandle>(null);
+
+  const [workerSelectsData, setWorkerSelectsData] = useState<IWorkerSelectsData>();
+
+  const { data: getEmployeesQueryData } = useGetEmployeesQuery({
+    onError: (error) => {
+      errorToast(error);
+    },
+  });
+
+  return (
+    <ProtectedTabTemplate>
+      {getEmployeesQueryData ? (
+        <WorkerSelects
+          ref={workerSelectsRef}
+          getEmployeesQueryData={getEmployeesQueryData}
+          setWorkerSelectsData={setWorkerSelectsData}
+        />
+      ) : (
+        <Spinner />
+      )}
+      {workerSelectsData && <Text>Selected</Text>}
+    </ProtectedTabTemplate>
+  );
+};
 
 export default PrintingSummariesTab;
--- src/tabs/SalariesTab.tsx
+++ src/tabs/SalariesTab.tsx
@@ -1,60 +1,30 @@
-import { useEffect, useRef, useState } from 'react';
-import { Box, Flex, Spinner, Table, TableContainer, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/react';
+import { Box, Flex, Table, TableContainer, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/react';
 
 import ProtectedTabTemplate from 'templates/ProtectedTabTemplate';
-import { useGetEmployeesQuery } from 'graphql/generated/schema';
-import { useErrorToast } from 'hooks';
-import WorkerSelects, { IWorkerSelectsData, WorkerSelectsHandle } from 'components/WorkerSelects/WorkerSelects';
 
-const SalariesTab = () => {
-  const errorToast = useErrorToast();
-  const workerSelectsRef = useRef<WorkerSelectsHandle>(null);
-
-  const [workerSelectsData, setWorkerSelectsData] = useState<IWorkerSelectsData>();
-
-  const { data: getEmployeesQueryData } = useGetEmployeesQuery({
-    onError: (error) => {
-      errorToast(error);
-    },
-  });
-
-  useEffect(() => {
-    console.log(workerSelectsData);
-  }, [workerSelectsData]);
-
-  return (
-    <ProtectedTabTemplate>
-      <Flex direction="column" gap={6}>
-        {getEmployeesQueryData ? (
-          <WorkerSelects
-            ref={workerSelectsRef}
-            getEmployeesQueryData={getEmployeesQueryData}
-            setWorkerSelectsData={setWorkerSelectsData}
-          />
-        ) : (
-          <Spinner />
-        )}
-        <Box w="full" overflowX="auto" bgColor="white" borderWidth={1} rounded="md">
-          <TableContainer>
-            <Table variant="striped">
-              <Thead>
-                <Tr>
-                  <Th>Pracownik</Th>
-                  <Th isNumeric>Wypłata</Th>
-                </Tr>
-              </Thead>
-              <Tbody>
-                <Tr>
-                  <Td>Jacek Nowak</Td>
-                  <Td isNumeric>1000,00 zł</Td>
-                </Tr>
-              </Tbody>
-            </Table>
-          </TableContainer>
-        </Box>
-      </Flex>
-    </ProtectedTabTemplate>
-  );
-};
+const SalariesTab = () => (
+  <ProtectedTabTemplate>
+    <Flex direction="column" gap={6}>
+      <Box w="full" overflowX="auto" bgColor="white" borderWidth={1} rounded="md">
+        <TableContainer>
+          <Table variant="striped">
+            <Thead>
+              <Tr>
+                <Th>Pracownik</Th>
+                <Th isNumeric>Wypłata</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td>Jacek Nowak</Td>
+                <Td isNumeric>1000,00 zł</Td>
+              </Tr>
+            </Tbody>
+          </Table>
+        </TableContainer>
+      </Box>
+    </Flex>
+  </ProtectedTabTemplate>
+);
 
 export default SalariesTab;
