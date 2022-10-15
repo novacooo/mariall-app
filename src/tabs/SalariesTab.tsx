48a8780dff81408cce560036807bbd7b849a7e91
Jacek Nowak <jaceknowak226@gmail.com>
2022-10-16 00:11:41 +0200

    Salaries table bg color fix

--- src/tabs/SalariesTab.tsx
+++ src/tabs/SalariesTab.tsx
@@ -1,30 +1,34 @@
-import { Box, Flex, Table, TableContainer, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/react';
+import { Box, Flex, Table, TableContainer, Tbody, Td, Th, Thead, Tr, useColorModeValue } from '@chakra-ui/react';
 
 import ProtectedTabTemplate from 'templates/ProtectedTabTemplate';
 
-const SalariesTab = () => (
-  <ProtectedTabTemplate>
-    <Flex direction="column" gap={6}>
-      <Box w="full" overflowX="auto" bgColor="white" borderWidth={1} rounded="md">
-        <TableContainer>
-          <Table variant="striped">
-            <Thead>
-              <Tr>
-                <Th>Pracownik</Th>
-                <Th isNumeric>Wypłata</Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td>Jacek Nowak</Td>
-                <Td isNumeric>1000,00 zł</Td>
-              </Tr>
-            </Tbody>
-          </Table>
-        </TableContainer>
-      </Box>
-    </Flex>
-  </ProtectedTabTemplate>
-);
+const SalariesTab = () => {
+  const bgColor = useColorModeValue('white', 'gray.800');
+
+  return (
+    <ProtectedTabTemplate>
+      <Flex direction="column" gap={6}>
+        <Box w="full" overflowX="auto" bgColor={bgColor} borderWidth={1} rounded="md">
+          <TableContainer>
+            <Table variant="striped">
+              <Thead>
+                <Tr>
+                  <Th>Pracownik</Th>
+                  <Th isNumeric>Wypłata</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                <Tr>
+                  <Td>Jacek Nowak</Td>
+                  <Td isNumeric>1000,00 zł</Td>
+                </Tr>
+              </Tbody>
+            </Table>
+          </TableContainer>
+        </Box>
+      </Flex>
+    </ProtectedTabTemplate>
+  );
+};
 
 export default SalariesTab;
