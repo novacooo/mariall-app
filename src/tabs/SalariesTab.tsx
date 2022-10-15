dbe8c8686acc9ae98c802be4679645c9d1cef12c
Jacek Nowak <jaceknowak226@gmail.com>
2022-10-15 13:04:05 +0200

    Salaries table skeleton

--- src/tabs/SalariesTab.tsx
+++ src/tabs/SalariesTab.tsx
@@ -1,10 +1,36 @@
 import ProtectedTabTemplate from 'templates/ProtectedTabTemplate';
+import { Box, Flex, Table, TableContainer, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/react';
 
 const SalariesTab = () => (
   <ProtectedTabTemplate>
-    <div>
-      <p>salaries-tab</p>
-    </div>
+    <Flex direction="column" gap={6}>
+      <Box w="full" overflowX="auto" bgColor="white" borderWidth={1} rounded="md">
+        <TableContainer>
+          <Table variant="striped">
+            <Thead>
+              <Tr>
+                <Th>Pracownik</Th>
+                <Th isNumeric>Wypłata</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td>Jacek Nowak</Td>
+                <Td isNumeric>5434,33 zł</Td>
+              </Tr>
+              <Tr>
+                <Td>Krzysztof Nowak</Td>
+                <Td isNumeric>8546,99 zł</Td>
+              </Tr>
+              <Tr>
+                <Td>Justyna Nowak</Td>
+                <Td isNumeric>2534,32 zł</Td>
+              </Tr>
+            </Tbody>
+          </Table>
+        </TableContainer>
+      </Box>
+    </Flex>
   </ProtectedTabTemplate>
 );
 
