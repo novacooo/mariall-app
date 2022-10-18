2dbdd8fd4ade9ce7faf222034aa773f3dbca245e
Jacek Nowak <jaceknowak226@gmail.com>
2022-10-19 00:43:58 +0200

    Conditions

--- src/tabs/SalariesTab.tsx
+++ src/tabs/SalariesTab.tsx
@@ -51,74 +51,88 @@ const SalariesTab = () => {
 
   return (
     <ProtectedTabTemplate>
-      <Flex
-        wrap="wrap"
-        gap={{
-          base: 3,
-          md: 4,
-        }}
-        direction={{
-          base: 'column',
-          md: 'row',
-        }}
-      >
-        <Menu>
-          <MenuButton as={Button} rightIcon={<ChevronDownIcon />} color={selectedYear ? selectAccentText : undefined}>
-            {selectedYear || t('selects.chooseYear')}
-          </MenuButton>
-          <MenuList maxH={60} overflow="hidden" overflowY="auto">
-            <MenuOptionGroup type="radio">
-              {years.map((year) => (
-                <MenuItemOption key={year} value={`${year}`} onClick={() => setSelectedYear(year)}>
-                  {year}
-                </MenuItemOption>
-              ))}
-            </MenuOptionGroup>
-          </MenuList>
-        </Menu>
-        <Menu>
-          <MenuButton as={Button} rightIcon={<ChevronDownIcon />} color={selectedMonth ? selectAccentText : undefined}>
-            {selectedMonth ? t(`months.${selectedMonth.name}`) : t('selects.chooseMonth')}
-          </MenuButton>
-          <MenuList maxH={60} overflow="hidden" overflowY="auto">
-            <MenuOptionGroup type="radio">
-              {months.map((month) => (
-                <MenuItemOption key={month.name} value={month.name} onClick={() => setSelectedMonth(month)}>
-                  {t(`months.${month.name}`)}
-                </MenuItemOption>
-              ))}
-            </MenuOptionGroup>
-          </MenuList>
-        </Menu>
-      </Flex>
       {employeesData ? (
-        <Box w="full" overflowX="auto" bgColor={bgColor} borderWidth={1} rounded="md">
-          <TableContainer>
-            <Table variant="striped">
-              <Thead>
-                <Tr>
-                  <Th>Pracownik</Th>
-                  <Th isNumeric>Wypłata</Th>
-                </Tr>
-              </Thead>
-              <Tbody>
-                {employeesData.map(({ id, attributes }) => {
-                  if (!id || !attributes) return null;
-
-                  const { firstName, lastName } = attributes;
-                  const workerName = lastName ? `${firstName} ${lastName}` : firstName;
-
-                  return (
-                    <Tr key={id}>
-                      <Td>{workerName}</Td>
-                      <Td isNumeric>1000,00 zł</Td>
+        <>
+          <Flex
+            wrap="wrap"
+            gap={{
+              base: 3,
+              md: 4,
+            }}
+            direction={{
+              base: 'column',
+              md: 'row',
+            }}
+          >
+            <Menu>
+              <MenuButton
+                as={Button}
+                rightIcon={<ChevronDownIcon />}
+                color={selectedYear ? selectAccentText : undefined}
+              >
+                {selectedYear || t('selects.chooseYear')}
+              </MenuButton>
+              <MenuList maxH={60} overflow="hidden" overflowY="auto">
+                <MenuOptionGroup type="radio">
+                  {years.map((year) => (
+                    <MenuItemOption key={year} value={`${year}`} onClick={() => setSelectedYear(year)}>
+                      {year}
+                    </MenuItemOption>
+                  ))}
+                </MenuOptionGroup>
+              </MenuList>
+            </Menu>
+            {selectedYear && (
+              <Menu>
+                <MenuButton
+                  as={Button}
+                  rightIcon={<ChevronDownIcon />}
+                  color={selectedMonth ? selectAccentText : undefined}
+                >
+                  {selectedMonth ? t(`months.${selectedMonth.name}`) : t('selects.chooseMonth')}
+                </MenuButton>
+                <MenuList maxH={60} overflow="hidden" overflowY="auto">
+                  <MenuOptionGroup type="radio">
+                    {months.map((month) => (
+                      <MenuItemOption key={month.name} value={month.name} onClick={() => setSelectedMonth(month)}>
+                        {t(`months.${month.name}`)}
+                      </MenuItemOption>
+                    ))}
+                  </MenuOptionGroup>
+                </MenuList>
+              </Menu>
+            )}
+          </Flex>
+          {selectedYear && selectedMonth && (
+            <Box w="full" overflowX="auto" bgColor={bgColor} borderWidth={1} rounded="md">
+              <TableContainer>
+                <Table variant="striped">
+                  <Thead>
+                    <Tr>
+                      <Th>Pracownik</Th>
+                      <Th isNumeric>Wypłata</Th>
                     </Tr>
-                  );
-                })}
-              </Tbody>
-            </Table>
-          </TableContainer>
-        </Box>
+                  </Thead>
+                  <Tbody>
+                    {employeesData.map(({ id, attributes }) => {
+                      if (!id || !attributes) return null;
+
+                      const { firstName, lastName } = attributes;
+                      const workerName = lastName ? `${firstName} ${lastName}` : firstName;
+
+                      return (
+                        <Tr key={id}>
+                          <Td>{workerName}</Td>
+                          <Td isNumeric>1000,00 zł</Td>
+                        </Tr>
+                      );
+                    })}
+                  </Tbody>
+                </Table>
+              </TableContainer>
+            </Box>
+          )}
+        </>
       ) : (
         <Spinner />
       )}
